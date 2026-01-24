236f5cbfaa6c6a2894d355978c36c5914e6416f6
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2026-01-24 13:35:50 -0800

    bumped version - minor code fix

--- resources/leiningen/new/webgen/scaffold-enhancement.clj
+++ resources/leiningen/new/webgen/scaffold-enhancement.clj
@@ -1,5 +1,7 @@
 (ns {{sanitized}}.scaffold.enhancement
-  "Auto-TabGrid enhancement for existing scaffold system")
+  "Auto-TabGrid enhancement for existing scaffold system"
+  (:require [br.engine.menu :as menu]
+            [clojure.edn :as edn]))
 
 (defn should-use-tabgrid
   "Auto-detect if entity should use TabGrid UI"
@@ -16,21 +18,30 @@
         (update-in [:ui] merge {:split-view true :breadcrumbs true}))
     entity-config))
 
+(defn load-entity-config
+  "Load entity configuration"
+  [entity-keyword]
+  (try
+    (edn/read-string (slurp (str "resources/entities/" (name entity-keyword) ".edn")))
+    (catch Exception e
+      (println (str "Error loading " (name entity-keyword) ":" (.getMessage e)))
+      {})))
+
 (defn check-tabgrid-status
   "Check TabGrid status for all entities"
   []
   (doseq [entity [:propiedades :alquileres :clientes :agentes]]
     (let [config (load-entity-config entity)]
       (when config
-        (println (str (name entity) ": " 
-                     (if (should-use-tabgrid config) 
-                       "TabGrid UI" 
-                       "Regular Grid UI")))))))
+        (println (str (name entity) ": "
+                      (if (should-use-tabgrid config)
+                        "TabGrid UI"
+                        "Regular Grid UI")))))))
 
 (defn get-tabgrid-entities
   "Get list of entities that will use TabGrid UI"
   []
-  (filter should-use-tabgrid 
+  (filter should-use-tabgrid
           (map load-entity-config [:propiedades :alquileres :clientes :agentes])))
 
 (defn integrate-tabgrid-into-scaffold
@@ -39,16 +50,6 @@
   {:integrated true
    :tabgrid-entities (get-tabgrid-entities)
    :changes-required false})
-
-(defn load-entity-config
-  "Load entity configuration"
-  [entity-keyword]
-  (try
-    (require 'clojure.edn :as edn)
-    (edn/read-string (slurp (str "resources/entities/" (name entity-keyword) ".edn")))
-    (catch Exception e
-      (println (str "Error loading " (name entity-keyword) ":" (.getMessage e)))
-      {})))
 
 (defn test-enhancement
   "Test the enhancement system"
