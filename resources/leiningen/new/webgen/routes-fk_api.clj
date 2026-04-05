2be81d961d57833c61219ef75cc0f264d6e6eb26
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2026-04-05 13:51:56 -0700

    bumped up version and fixed bug in routes

--- resources/leiningen/new/webgen/routes-fk_api.clj
+++ resources/leiningen/new/webgen/routes-fk_api.clj
@@ -182,7 +182,7 @@
                                fields)
               ;; render the fields using the same server-side helper; pass empty row
               ;; we reference the private var via var literal to avoid visibility errors
-              rendered (let [render-fn #'br.engine.render/render-field]
+              rendered (let [render-fn #'{{sanitized}}.engine.render/render-field]
                          (->> fields
                               (map #(render-fn % {}))
                               (html)))]
