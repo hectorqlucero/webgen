449cf0d0887bf8ef655764fe8f2440c3bafd6e0d
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2026-04-26 13:12:15 -0700

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.hector/lein-template.webgen "0.3.49"
+(defproject org.clojars.hector/lein-template.webgen "0.3.50"
   :description "WebGen/LST Parameter-Driven Web Application Template"
   :url "https://github.com/hectorqlucero/webgen"
   :license {:name "MIT License"
